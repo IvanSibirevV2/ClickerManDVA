a3aac89d193ad9404ad517983e65fef8a1580cb1
qwe <qwe>
2023-03-09 14:25:24 +0300

    Таймер

--- ClickerManDVA/ClickerManDVA_TestEr/MainWindow.xaml.cs
+++ ClickerManDVA/ClickerManDVA_TestEr/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+///////////////////////////////////////////////
+using System.Threading;
+
 
 namespace ClickerManDVA_TestEr
 {
@@ -20,9 +23,38 @@ namespace ClickerManDVA_TestEr
     /// </summary>
     public partial class MainWindow : Window
     {
+        public System.Threading.Thread p_Thread = null;
+        public System.DateTime p_DateTime = System.DateTime.Now;
+        public System.TimeSpan p_TimeSpan = System.TimeSpan.Zero;
         public MainWindow()
         {
+            this.p_Thread= new Thread(() => {
+                while (true)
+                {
+                    System.TimeSpan _TimeSpan_Now = System.DateTime.Now - this.p_DateTime;
+                    if ((_TimeSpan_Now - this.p_TimeSpan).TotalMilliseconds > 100)
+                    {
+                        this.p_TimeSpan = _TimeSpan_Now;
+                        Dispatcher.InvokeAsync(() => this.Title = this.p_TimeSpan.TotalMilliseconds.ToString());
+                    }
+                    
+                }
+                
+                //System.Threading.Thread.Sleep(100);
+            });
             InitializeComponent();
+            this.p_Thread.Start();
+            
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            this.p_Thread.Abort();
+            MessageBox.Show("Close Application?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Warning);
         }
     }
 }
