ce98239390289ddb2434c6399378f8e883bed057
IVS <IVS@IVS>
2023-03-15 13:38:04 +0300

    Htafrnjhbyu

---
+++ Прошлое/ClickerManDVA/ClickerManDVA/Klava2.cs
@@ -0,0 +1,148 @@
+﻿
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+using System.Linq;
+
+namespace System
+{
+    public partial class Klava
+    {
+        public Klava A{get{this.VK_A.Down().Sleep().Up(); return this;} }
+        public Klava B{get{this.VK_B.Down().Sleep().Up(); return this;} }
+        public Klava C{get{this.VK_C.Down().Sleep().Up(); return this;} }
+        public Klava D{get{this.VK_D.Down().Sleep().Up(); return this;} }
+        public Klava E{get{this.VK_E.Down().Sleep().Up(); return this;} }
+        public Klava F{get{this.VK_F.Down().Sleep().Up(); return this;} }
+        public Klava G{get{this.VK_G.Down().Sleep().Up(); return this;} }
+        public Klava H{get{this.VK_H.Down().Sleep().Up(); return this;} }
+        public Klava I{get{this.VK_I.Down().Sleep().Up(); return this;} }
+        public Klava J{get{this.VK_J.Down().Sleep().Up(); return this;} }
+        public Klava K{get{this.VK_K.Down().Sleep().Up(); return this;} }
+        public Klava L{get{this.VK_L.Down().Sleep().Up(); return this;} }
+        public Klava M{get{this.VK_M.Down().Sleep().Up(); return this;} }
+        public Klava N{get{this.VK_N.Down().Sleep().Up(); return this;} }
+        public Klava O{get{this.VK_O.Down().Sleep().Up(); return this;} }
+        public Klava P{get{this.VK_P.Down().Sleep().Up(); return this;} }
+        public Klava Q{get{this.VK_Q.Down().Sleep().Up(); return this;} }
+        public Klava R{get{this.VK_R.Down().Sleep().Up(); return this;} }
+        public Klava S{get{this.VK_S.Down().Sleep().Up(); return this;} }
+        public Klava T{get{this.VK_T.Down().Sleep().Up(); return this;} }
+        public Klava U{get{this.VK_U.Down().Sleep().Up(); return this;} }
+        public Klava V{get{this.VK_V.Down().Sleep().Up(); return this;} }
+        public Klava W{get{this.VK_W.Down().Sleep().Up(); return this;} }
+        public Klava X{get{this.VK_X.Down().Sleep().Up(); return this;} }
+        public Klava Y{get{this.VK_Y.Down().Sleep().Up(); return this;} }
+        public Klava Z{get{this.VK_Z.Down().Sleep().Up(); return this;} }
+        public Klava _0{get{this.VK__0.Down().Sleep().Up(); return this;} }
+        public Klava _1{get{this.VK__1.Down().Sleep().Up(); return this;} }
+        public Klava _2{get{this.VK__2.Down().Sleep().Up(); return this;} }
+        public Klava _3{get{this.VK__3.Down().Sleep().Up(); return this;} }
+        public Klava _4{get{this.VK__4.Down().Sleep().Up(); return this;} }
+        public Klava _5{get{this.VK__5.Down().Sleep().Up(); return this;} }
+        public Klava _6{get{this.VK__6.Down().Sleep().Up(); return this;} }
+        public Klava _7{get{this.VK__7.Down().Sleep().Up(); return this;} }
+        public Klava _8{get{this.VK__8.Down().Sleep().Up(); return this;} }
+        public Klava _9{get{this.VK__9.Down().Sleep().Up(); return this;} }
+        public Klava Slash{get{this.VK_Slash.Down().Sleep().Up(); return this;} }
+        public Klava Space{get{this.VK_Space.Down().Sleep().Up(); return this;} }
+        public Klava Enter{get{this.VK_Enter.Down().Sleep().Up(); return this;} }
+        public Klava Mark{get{this.VK_Mark.Down().Sleep().Up(); return this;} }
+        public Klava Shift{get{this.VK_Shift.Down().Sleep().Up(); return this;} }
+        public Klava F1{get{this.VK_F1.Down().Sleep().Up(); return this;} }
+        public Klava F2{get{this.VK_F2.Down().Sleep().Up(); return this;} }
+        public Klava F3{get{this.VK_F3.Down().Sleep().Up(); return this;} }
+        public Klava F4{get{this.VK_F4.Down().Sleep().Up(); return this;} }
+        public Klava F5{get{this.VK_F5.Down().Sleep().Up(); return this;} }
+        public Klava F6{get{this.VK_F6.Down().Sleep().Up(); return this;} }
+        public Klava F7{get{this.VK_F7.Down().Sleep().Up(); return this;} }
+        public Klava F8{get{this.VK_F8.Down().Sleep().Up(); return this;} }
+        public Klava F9{get{this.VK_F9.Down().Sleep().Up(); return this;} }
+        public Klava F10{get{this.VK_F10.Down().Sleep().Up(); return this;} }
+        public Klava F11{get{this.VK_F11.Down().Sleep().Up(); return this;} }
+        public Klava F12{get{this.VK_F12.Down().Sleep().Up(); return this;} }
+        public Klava BACKSPACE{get{this.VK_BACKSPACE.Down().Sleep().Up(); return this;} }
+        public Klava TAB{get{this.VK_TAB.Down().Sleep().Up(); return this;} }
+        public Klava CTRL{get{this.VK_CTRL.Down().Sleep().Up(); return this;} }
+        public Klava ALT{get{this.VK_ALT.Down().Sleep().Up(); return this;} }
+        public Klava PAUSE{get{this.VK_PAUSE.Down().Sleep().Up(); return this;} }
+        public Klava CAPSLOCK{get{this.VK_CAPSLOCK.Down().Sleep().Up(); return this;} }
+        public Klava ESCAPE{get{this.VK_ESCAPE.Down().Sleep().Up(); return this;} }
+        public Klava PAGEUP{get{this.VK_PAGEUP.Down().Sleep().Up(); return this;} }
+        public Klava PAGEDOWN{get{this.VK_PAGEDOWN.Down().Sleep().Up(); return this;} }
+        public Klava END{get{this.VK_END.Down().Sleep().Up(); return this;} }
+        public Klava HOME{get{this.VK_HOME.Down().Sleep().Up(); return this;} }
+        public Klava LEFT{get{this.VK_LEFT.Down().Sleep().Up(); return this;} }
+        public Klava UP{get{this.VK_UP.Down().Sleep().Up(); return this;} }
+        public Klava RIGHT{get{this.VK_RIGHT.Down().Sleep().Up(); return this;} }
+        public Klava DOWN{get{this.VK_DOWN.Down().Sleep().Up(); return this;} }
+        public Klava INSERT{get{this.VK_INSERT.Down().Sleep().Up(); return this;} }
+        public Klava DELETE{get{this.VK_DELETE.Down().Sleep().Up(); return this;} }
+        public Klava LWIN{get{this.VK_LWIN.Down().Sleep().Up(); return this;} }
+        public Klava RWIN{get{this.VK_RWIN.Down().Sleep().Up(); return this;} }
+        public Klava NumZero{get{this.VK_NumZero.Down().Sleep().Up(); return this;} }
+        public Klava NumOne{get{this.VK_NumOne.Down().Sleep().Up(); return this;} }
+        public Klava NumTwo{get{this.VK_NumTwo.Down().Sleep().Up(); return this;} }
+        public Klava NumThree{get{this.VK_NumThree.Down().Sleep().Up(); return this;} }
+        public Klava NumFor{get{this.VK_NumFor.Down().Sleep().Up(); return this;} }
+        public Klava NumFive{get{this.VK_NumFive.Down().Sleep().Up(); return this;} }
+        public Klava NumSix{get{this.VK_NumSix.Down().Sleep().Up(); return this;} }
+        public Klava NumSeven{get{this.VK_NumSeven.Down().Sleep().Up(); return this;} }
+        public Klava NumEight{get{this.VK_NumEight.Down().Sleep().Up(); return this;} }
+        public Klava NumNine{get{this.VK_NumNine.Down().Sleep().Up(); return this;} }
+        public Klava NumStar{get{this.VK_NumStar.Down().Sleep().Up(); return this;} }
+        public Klava NumPlus{get{this.VK_NumPlus.Down().Sleep().Up(); return this;} }
+        public Klava NumMinus{get{this.VK_NumMinus.Down().Sleep().Up(); return this;} }
+        public Klava NumDot{get{this.VK_NumDot.Down().Sleep().Up(); return this;} }
+        public Klava NumSlash{get{this.VK_NumSlash.Down().Sleep().Up(); return this;} }
+        public Klava NUMLOCK{get{this.VK_NUMLOCK.Down().Sleep().Up(); return this;} }
+        public Klava SCROLLLOCK{get{this.VK_SCROLLLOCK.Down().Sleep().Up(); return this;} }
+        public Klava PRINTSCREEN{get{this.VK_PRINTSCREEN.Down().Sleep().Up(); return this;} }
+        public Klava PeriodComma{get{this.VK_PeriodComma.Down().Sleep().Up(); return this;} }
+        public Klava Equally{get{this.VK_Equally.Down().Sleep().Up(); return this;} }
+        public Klava Сomma{get{this.VK_Сomma.Down().Sleep().Up(); return this;} }
+        public Klava Minus{get{this.VK_Minus.Down().Sleep().Up(); return this;} }
+        public Klava Dot{get{this.VK_Dot.Down().Sleep().Up(); return this;} }
+        public Klava Tilda{get{this.VK_Tilda.Down().Sleep().Up(); return this;} }
+        public Klava SquScobOtk{get{this.VK_SquScobOtk.Down().Sleep().Up(); return this;} }
+        public Klava ObratSlash{get{this.VK_ObratSlash.Down().Sleep().Up(); return this;} }
+        public Klava SquScobZak{get{this.VK_SquScobZak.Down().Sleep().Up(); return this;} }
+        public Klava MouseL{get{this.VK_MouseL.Down().Sleep().Up(); return this;} }
+        public Klava MouseR{get{this.VK_MouseR.Down().Sleep().Up(); return this;} }
+        public Klava MMouse{get{this.VK_MMouse.Down().Sleep().Up(); return this;} }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
