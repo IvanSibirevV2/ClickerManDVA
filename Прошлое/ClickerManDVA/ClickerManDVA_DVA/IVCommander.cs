7fe93dab78bc06c4af9523fdeb699872dcd522e0
IVS <IVS@IVS>
2023-03-23 10:27:19 +0300

    tttt

--- Прошлое/ClickerManDVA/ClickerManDVA_DVA/IVCommander.cs
+++ Прошлое/ClickerManDVA/ClickerManDVA_DVA/IVCommander.cs
@@ -6,17 +6,64 @@ using System.Threading.Tasks;
 
 namespace ClickerManDVA_DVA
 {
-    public interface IVCommander: INewEable,ICloneable 
+    public interface IVCommander : INewEable, ICloneable
     {
+        System.Byte p_nVirtKey { get; set; }
+        System.String p_Str { get; set; }
+        System.TimeSpan p_TimeSpan { get; set; }
         //INewEable
         //object New();
         //ICloneable
         //object Clone();
-        string p_Str { get; set; }
-        System.TimeSpan p_TimeSpan { get; set; }
-        IVCommander Set_TimeSpan(System.TimeSpan _TimeSpan);
+        IVCommander Set(IVCommander _this = null
+            , System.Nullable<System.Byte> _nVirtKey = null
+            , System.String _Str = null
+            , System.Nullable<System.TimeSpan> _TimeSpan = null
+        );
         System.Boolean Is();
-        VKeyboard GetAsVKeyboard();
-        VMouse GetAsVMouse();
+    }
+    public class VCommander : IVCommander
+    {
+        private System.Byte p__nVirtKey = 65;//A
+        public System.Byte p_nVirtKey { get { return this.p__nVirtKey; } set { this.p__nVirtKey = value; } }
+        private System.String p__Str = "A";
+        public System.String p_Str { get { return this.p__Str; } set { this.p__Str = value; } }
+        private System.TimeSpan p__TimeSpan;
+        public System.TimeSpan p_TimeSpan { get {  return this.p__TimeSpan; } set { this.p__TimeSpan = value; } }
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+        public VCommander() { }
+        public VCommander Set(VCommander _this =null
+            , System.Nullable<System.Byte> _nVirtKey =null
+            , System.String _Str=null
+            , System.Nullable<System.TimeSpan> _TimeSpan=null
+        ) 
+        {
+            if (_this != null) _this.Set(_this: null, _nVirtKey: _this.p__nVirtKey, _Str: _this.p__Str, _TimeSpan: _this.p__TimeSpan);
+            if (_nVirtKey != null) this.p__nVirtKey = _nVirtKey.Value;
+            if(_Str!=null) this.p__Str= _Str;
+            if (_TimeSpan != null) this.p__TimeSpan = _TimeSpan.Value;
+            return this;
+        }
+        IVCommander IVCommander.Set(IVCommander _this = null
+            , System.Nullable<System.Byte> _nVirtKey = null
+            , System.String _Str = null
+            , System.Nullable<System.TimeSpan> _TimeSpan = null
+        )
+        {
+            if (_this != null) _this.Set(_this: null, _nVirtKey: _this.p_nVirtKey, _Str: _this.p_Str, _TimeSpan: _this.p_TimeSpan);
+            if (_nVirtKey != null) this.p__nVirtKey = _nVirtKey.Value;
+            if (_Str != null) this.p__Str = _Str;
+            if (_TimeSpan != null) this.p__TimeSpan = _TimeSpan.Value;
+            return this;
+        }
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+        public VCommander New() { return new VCommander(); }
+        System.Object INewEable.New() { return new VCommander(); }
+        public VCommander Clone() { return this.New().Set(_this:this); }
+        System.Object ICloneable.Clone() { return this.New().Set(_this: this); }
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////
+        System.Boolean IVCommander.Is()
+        { return this.Is(); }
+        public System.Func<System.Boolean> Is = () =>{ return false; };
     }
 }
--- Прошлое/ClickerManDVA/ClickerManDVA_DVA/VKeyboard.cs
+++ Прошлое/ClickerManDVA/ClickerManDVA_DVA/VKeyboard.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace ClickerManDVA_DVA
 {
-    public class VKeyboard: IVCommander
+    public class VKeyboard : VCommander, IVCommander
     {
         [DllImport("user32.dll")]
         private static extern short GetAsyncKeyState(int vKey);
@@ -17,42 +17,31 @@ namespace ClickerManDVA_DVA
         [DllImport("user32.dll")]
         private static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
         //////////////////////////////////////////////////////////////////////////////////////////
-        public byte p_nVirtKey = 65;//A
-        public string p__Str = "A";
-        public string p_Str { get { return this.p__Str; } set { this.p__Str = value; } }
-        public System.TimeSpan p__TimeSpan;
-        public System.TimeSpan p_TimeSpan { get { return this.p__TimeSpan; } set { this.p__TimeSpan = value; } }
-        //////////////////////////////////////////////////////////////////////////////////////////
-        public VKeyboard() {}
+        public VKeyboard() 
+        {
+            this.Is = () => {
+                switch (GetKeyState(this.p_nVirtKey)) { case -127:case -128:return true; break; default:return false; break;}
+                return false;
+            };
+        }
         public VKeyboard Set(
-            VKeyboard _this=null
+            VKeyboard _this = null
             , System.Nullable<System.Byte> _nVirtKey = null
             , System.String _Str = null
             , System.Nullable<System.TimeSpan> _TimeSpan = null
             )
         {
-            if (_this != null) this.Set(_this:null, _nVirtKey:_this.p_nVirtKey,_Str:_this.p__Str,_TimeSpan:_this.p_TimeSpan);
-            if (_nVirtKey!=null) this.p_nVirtKey = _nVirtKey.Value;
-            if (_Str != null) this.p__Str = _Str;
-            if (_TimeSpan!=null) this.p_TimeSpan = _TimeSpan.Value;
+            if (_this != null) this.Set(_this: null, _nVirtKey: _this.p_nVirtKey, _Str: _this.p_Str, _TimeSpan: _this.p_TimeSpan);
+            if (_nVirtKey != null) this.p_nVirtKey = _nVirtKey.Value;
+            if (_Str != null) this.p_Str = _Str;
+            if (_TimeSpan != null) this.p_TimeSpan = _TimeSpan.Value;
             return this;
         }
         //////////////////////////////////////////////////////////////////////////////////////////
-        IVCommander IVCommander.Set_TimeSpan(System.TimeSpan _TimeSpan) { return this.Set(_TimeSpan:_TimeSpan);}
-        public VKeyboard Set_TimeSpan(System.TimeSpan _TimeSpan){return this.Set(_TimeSpan: _TimeSpan);}
         System.Object INewEable.New() { return this.New(); }
         public VKeyboard New() { return new VKeyboard(); }
         System.Object ICloneable.Clone() { return this.Clone(); }
-        public VKeyboard Clone() {return this.New().Set(_this:this);}
+        public VKeyboard Clone() { return this.New().Set(_this: this); }
         //////////////////////////////////////////////////////////////////////////////////////////
-        System.Boolean IVCommander.Is()
-        { switch (GetKeyState(this.p_nVirtKey)) { case -127: case -128: return true; break; default: return false; break; } return false; }
-        public System.Boolean Is()
-        { switch (GetKeyState(this.p_nVirtKey)){ case -127: case -128: return true; break; default: return false; break; } return false; }
-        //////////////////////////////////////////////////////////////////////////////////////////
-        VKeyboard IVCommander.GetAsVKeyboard() {return this;}
-        public VKeyboard GetAsVCommander() { return this; }
-        VMouse IVCommander.GetAsVMouse() { return null; }
-        public VMouse GetAsVMouse() { return null; }
     }
 }
