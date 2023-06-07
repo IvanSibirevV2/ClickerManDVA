3a3058697573c751c79384fddb188ab147339dd5
IVS <IVS@IVS>
2023-06-07 12:12:02 +0300

    AutoCommit_2023.06.07_12:12:01_SibNout2020

---
+++ _LN_AFRAU_Wpf_Diplom/Pages/PGMainMenu.xaml.cs
@@ -0,0 +1,120 @@
+﻿#region Юзинги
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WindowsInput.Native;
+using WindowsInput;
+using static WpfDiplom.Pages.PGSetings;
+using System.Reflection.Emit;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Xml.Linq;
+using System.Windows.Threading;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+using Microsoft.Win32;
+using System.Runtime.CompilerServices;
+using ClickerManDVA_DVA;
+#endregion
+
+namespace WpfDiplom.Pages
+{
+    public partial class PGMainMenu : System.Windows.Controls.Page
+    {
+
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(int vKey);
+        [DllImport("user32.dll")]
+        private static extern short GetKeyState(int nVirtKey);
+        [DllImport("user32.dll")]
+        private static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        //WpfDiplom.Pages.PGMainMenu.p_DataContext
+        private static ClickerManDVA_DVA.DataContext p_DataContext = new ClickerManDVA_DVA.DataContext();
+        #region Библиотеки
+        //Подключение библиотек
+        /*
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [DllImport("User32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
+        */
+        #endregion
+
+
+        //Инициализация
+        public PGMainMenu()
+        {
+            InitializeComponent();
+        }
+        #region #######################################3
+        public PGMainMenu _Down(byte _bVk) { keybd_event(_bVk, 0, 0, 0); return this; }
+        public PGMainMenu _Up(byte _bVk) { keybd_event(_bVk, 0, 2, 0); return this; }
+        public System.Boolean _Is(byte _bVk) { switch (GetKeyState(_bVk)){ case -127: case -128: return true; break;default: return false; break;} return false; }
+        //////////////////////////////////////////////////////
+        //(65, "A")
+        public System.String A_String() { return "A"; }
+        public System.Byte A_ASCII() { return 65; }
+
+        public PGMainMenu A() { return this; }
+        public PGMainMenu A_Down() { return this._Down(this.A_ASCII()); }
+        public PGMainMenu A_Up() { return this._Up(this.A_ASCII()); }
+        public bool A_Is() { return this._Is(this.A_ASCII()); }
+        
+
+        //(65, "A")
+        //Опустить
+        // поднять 
+        // проверить
+        #endregion
+        private void Button_Click(object sender, RoutedEventArgs e){}
+        private void btn_StopRec_Click(object sender, RoutedEventArgs e){}
+        async private void btn_StartWork_Click(object sender, RoutedEventArgs e){}
+        #region Переход на страницу настроек
+        private void btn_Setings_Click(object sender, RoutedEventArgs e)
+        {NavigationService.Navigate(new PGSetings());}
+        private void Btn62_Click(object sender, RoutedEventArgs e){ }
+        #endregion
+        private void btn_AllDel_Click(object sender, RoutedEventArgs e){}
+        private void btn_Read_Click(object sender, RoutedEventArgs e){}
+        private void MouseBtnLeft_Click(object sender, RoutedEventArgs e){}
+        async private void btn_Test_Click(object sender, RoutedEventArgs e){}
+
+        private void Button_Click_1(object sender, RoutedEventArgs e){}
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Btn64_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
